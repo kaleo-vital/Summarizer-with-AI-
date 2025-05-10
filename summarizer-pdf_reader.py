125f3ab8588aa73f564459d21b8717153b4c62cf
kaleo-vital <kaleovital09@gmail.com>
2025-05-10 11:39:26 -0300

    Create summarizer-pdf_reader.py

---
+++ summarizer-pdf_reader.py
@@ -0,0 +1,9 @@
+import PyPDF2
+
+def extract_text_from_pdf(path):
+    text = ""
+    with open(path, "rb") as f:
+        reader = PyPDF2.PdfReader(f)
+        for page in reader.pages:
+            text += page.extract_text()
+    return text
